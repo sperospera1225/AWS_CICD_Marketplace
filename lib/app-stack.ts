2e748c5389abbfaabdcf4f30fc7a9bf44283730f
sperospera1225 <sperospera1225@gmail.com>
2022-11-07 12:05:03 +0000

    add agent layer with trend micro

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -19,5 +19,16 @@ export class AppStack extends cdk.Stack {
       handler: 'index.handler',
       code: lambda.Code.fromAsset('lambda/')
     });
+    
+		// Agent Layer 추가
+    const agentArn = 'arn:aws:lambda:ap-northeast-1:800880067056:layer:CloudOne-ApplicationSecurity-nodejs14_x:1';
+    const agentLayer = lambda.LayerVersion.fromLayerVersionArn(this, 'TrendMicroAgent', agentArn);
+    fn.addLayers(agentLayer)
+    
+    // 환경 변수 추가
+    fn.addEnvironment('AWS_LAMBDA_EXEC_WRAPPER', '/opt/trend_app_protect');
+    fn.addEnvironment('TREND_AP_HELLO_URL', 'https://agents.jp-1.application.cloudone.trendmicro.com/');
+    fn.addEnvironment('TREND_AP_KEY', 'bedb1ddc-d830-4841-839c-7b37ba747bac');
+    fn.addEnvironment('TREND_AP_SECRET', 'f809d8b6-e9d0-4008-839a-2333b2cbfb45');
   }
 }
