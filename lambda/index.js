3e3921a9e0ef0eda3c2f8329266b5b0b30d830e5
sperospera1225 <sperospera1225@gmail.com>
2022-11-07 12:09:46 +0000

    update lambda function with command injection

--- lambda/index.js
+++ lambda/index.js
@@ -1,13 +1,24 @@
+const { exec } = require('node:child_process');
+
 let response;
  
 exports.handler = async (event, context) => {
   try {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Hello Marketplace!",
-      }),
-    };
+    exec("ls -al", (err, stdout, stderr) => {
+        if (err) {
+            throw Error(`error: ${err.message}`);
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+        }
+        
+      response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Hello Marketplace!",
+        }),
+      };
+    });
   } catch (err) {
     console.log(err);
     return err;
